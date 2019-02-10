87ee0591aa0cea1e0785d3d032f22fb3af15af7f
varunswing <9204varun@gmail.com>
2019-02-11 00:00:26 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+    public static void main(String [] args) { //String [] args, String args[], String[] args all are same.
+
+        System.out.print("Hello, World!"); //System and String are predefined class as they are starting in capital.
+
+    }
+
+}
