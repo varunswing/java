f6947dc9d93994499fcd66415048b5cad63cc1bf
varunswing <9204varun@gmail.com>
2019-02-14 01:33:33 +0530

    Update Wrapper.java

--- Wrapper.java
+++ Wrapper.java
@@ -2,11 +2,16 @@ public class Wrapper
 {
     public static void main(String []args)
     {
-        Integer i1=Integer.valueof("101011",2);
-        Double d1=Double.valueof("3.14");
+        Integer i1=Integer.valueOf("101011",2);
+        Double d1=Double.valueOf("3.14");
         int a=Integer.parseInt("13");
         double b=Double.parseDouble("13.45");
-
-        int c=i1.intvalue();
+        int c=i1.intValue();
+        
+        System.out.println("C is "+c);
+        System.out.println("B is "+b);
+        System.out.println("A is "+a);
+        System.out.println("D1 is "+d1);
+        System.out.println("I1 is "+i1);
     }
 }
