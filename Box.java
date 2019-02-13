ccc41a6f3509289cf94968b0507a8f3a5b7bf982
varunswing <9204varun@gmail.com>
2019-02-14 01:34:13 +0530

    Update Box.java

--- Box.java
+++ Box.java
@@ -1,27 +1,26 @@
-class Box
+class Example
 {
     private int length, breadth, height;
     public void setDimension(int l,int b,int h)
     {
         length=l; breadth=b; height=h;
     }
-        public void showDimension()
-        {
-            System.out.println("L="+length);
-            System.out.println("b="+breadth);
-            System.out.println("h="+height); 
-        }
-  
-    }
-class Example
-{
-    public static void main(String[] args)
+
+    public void showDimension()
     {
-        Box smallBox = new Box();
-        smallBox.setDimension(12, 10, 5);
-        smallBox.showDimension();
-        smallBox = new Box();
-        smallBox.showDimension();
+        System.out.println("L="+length);
+        System.out.println("B="+breadth);
+        System.out.println("H="+height); 
+    }
+
+
+    static class Box {
+        public static void main(){ 
+            Example smallExample = new Example();
+            smallExample.setDimension(12, 10, 5);
+            smallExample.showDimension();
+            smallExample = new Example();
+            smallExample.showDimension();
+        }
     }
 }
-
