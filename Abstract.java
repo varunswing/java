4cefa0839720493a2649c1a03f19e59d58479bee
varunswing <9204varun@gmail.com>
2019-03-02 20:54:28 +0530

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,22 @@
+abstract class Human
+{
+    public abstract void walk();
+    public void eat()  
+    {
+
+    }
+}
+class Man extends Human
+{
+    public void walk()
+    {
+        System.out.println()
+    }
+}
+public class Abstract
+{
+    public static void main(String[] args)
+    {
+        Human obj = new Man();
+    }
+}
