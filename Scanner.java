426ee133ab916ef13d3fda4ac67d9bdf887b89a9
varunswing <9204varun@gmail.com>
2019-03-28 13:14:06 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,14 @@
+
+public class Scanner
+{
+    public static void main(String[] args)
+    {
+        int n=0;
+        System.out.println("Enter the number");
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(n);
+
+    }
+}
