8506f6043f892badb253854665b0241f59a67167
varunswing <9204varun@gmail.com>
2019-03-28 19:39:37 +0530

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInput
+{
+    public static void main(String[] args)
+    {
+        int n=0;
+        System.out.println("Enter the number");
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(n);
+
+    }
+}
