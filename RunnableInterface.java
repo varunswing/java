07f373ba0933860d32b79189cda341488a94b8dd
varunswing <varuns939@gmail.com>
2019-04-02 21:13:20 +0530

    Create RunnableInterface.java

---
+++ RunnableInterface.java
@@ -0,0 +1,48 @@
+class Hi implements Runnable
+{
+    public void run()
+    {
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println("Hi");
+            try
+            {
+                Thread.sleep(1000);
+            } catch (Exception e) 
+            {
+                
+            }
+        }
+    }
+}
+class Hello implements Runnable
+{
+    public void run() 
+    {
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println("Hello");
+        }        
+    }
+}
+public class RunnableInterface 
+{
+    public static void main(String[] args) 
+    {
+        Runnable obj1 = new Hi();
+        Runnable obj2 = new Hello();
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        try
+            {
+                Thread.sleep(10);
+            } catch (Exception e) 
+            {
+                
+            }
+        t2.start();
+    }    
+}
